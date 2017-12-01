5b68156b789d2117fe72967ddcdbc608c29ebd04
Ginny Munson <virginia.munson@gmail.com>
2017-12-01 22:39:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 function shout(string){
    return string.toUpperCase()
  }
- 
+
  function whisper(string){
    return string.toLowerCase()
  }
- 
+
  function logShout(){
    console.log('HELLO')
  }
- 
+
  function logWhisper(){
    console.log('hello')
  }
- 
+
  function sayHiToGrandma(string){
    if string === lowercase{
      return("I can't hear you!")
