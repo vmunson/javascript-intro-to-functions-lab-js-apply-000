1311f27cb462e7f146811ea15c8c502b7dce8713
Ginny Munson <virginia.munson@gmail.com>
2017-12-01 22:50:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ function shout(string){
  }
 debugger
  function sayHiToGrandma(string){
-   if (string.toLowerCase === lowercase){
+   if (string.toLowerCase === string){
      return("I can't hear you!")
    }
-   else if (string.toUpperCase === uppercase){
+   else if (string.toUpperCase === string){
      return("YES INDEED!")
    }
    else if (string === "I love you, Grandma"){
